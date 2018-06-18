ad94572d57bbdec69a538c017eff3f5e6a9f6bb6
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-18 22:59:07 +0400

    Create List Model
    Added struct List
    
    Co-Authored-By: Abhishek Verma <mr.abhishek.verma@outlook.com>

---
+++ FactsList/Model/List.swift
@@ -0,0 +1,16 @@
+//
+//  List.swift
+//  FactsList
+//
+//  Created by Abhishek Verma on 6/18/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import Foundation
+
+struct List {
+    let title : String
+    let describtion : String
+    let imageURL : String
+}
+
