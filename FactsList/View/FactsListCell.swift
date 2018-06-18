f6966ceb0429bdc101d325e41bd85e40e94ca53b
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-19 01:34:34 +0400

    Create table cell class

---
+++ FactsList/View/FactsListCell.swift
@@ -0,0 +1,28 @@
+//
+//  FactsListCell.swift
+//  FactsList
+//
+//  Created by Abhishek Verma on 6/19/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+class FactsListCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var describtionLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
