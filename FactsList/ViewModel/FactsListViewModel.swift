e10e758f058d77ffdbb59acc27fea516b2b2dd01
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-19 01:00:43 +0400

    Reverse file contents of FactsListViewModel and FactsListDataSource

--- FactsList/ViewModel/FactsListViewModel.swift
+++ FactsList/ViewModel/FactsListViewModel.swift
@@ -10,4 +10,35 @@ import Foundation
 
 struct FactsListViewModel {
     
+    weak var service: FactsListServiceProtocol?
+    
+    func fetchListDate(_ completion: ((Result<Bool, ErrorResult>) -> Void)? = nil) {
+        
+        guard let service = service else {
+            completion?(Result.failure(ErrorResult.custom(string: "Service is missing")))
+            return
+        }
+        
+        service.fetchFactsList { result in
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let list) :
+                    // reload data
+                    
+                    completion?(Result.success(true))
+                    
+                    break
+                case .failure(let error) :
+                    print("Parser error \(error)")
+                    completion?(Result.failure(error))
+                    
+                    break
+                }
+            }
+            
+        }
+    }
+    
+    
 }
