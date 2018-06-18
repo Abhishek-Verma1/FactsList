2aeff2c07ebbb774e9b9dda5ec23a8da93c1f67c
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-18 23:31:57 +0400

    Cretae ViewModel
    FactsListViewModel
    FactsListDataSource

---
+++ FactsList/ViewModel/FactsListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FactsListViewModel.swift
+//  FactsList
+//
+//  Created by Abhishek Verma on 6/18/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import Foundation
+
+struct FactsListViewModel {
+    
+}
