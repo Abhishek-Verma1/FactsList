49a30486c866dae7b6ac38f9b9476d0259b2bbaf
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-19 00:09:26 +0400

    Extend RequestService class functionality

--- FactsList/Service/Network/RequestService.swift
+++ FactsList/Service/Network/RequestService.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+final class RequestService {
+    
+    func loadData(urlString: String, session: URLSession = URLSession(configuration: .default), completion: @escaping (Result<Data, ErrorResult>) -> Void) -> URLSessionTask? {
+        
+        //Ignoring wrong URL
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.network(string: "URL format is wrong")))
+            return nil
+        }
+        
+        //Creating GET request
+        var request = RequestFactory.request(method: .GET, url: url)
+        
+        //Checking network connection
+        if let reachability = Reachability(), !reachability.isReachable {
+            request.cachePolicy = .returnCacheDataDontLoad
+        }
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //For failure case
+            if let error = error {
+                completion(.failure(.network(string: "An error occured during request :" + error.localizedDescription)))
+                return
+            }
+            
+            //For success case
+            if let data = data {
+                completion(.success(data))
+            }
+        }
+        task.resume()
+        return task
+        
+    }
+    
+}
