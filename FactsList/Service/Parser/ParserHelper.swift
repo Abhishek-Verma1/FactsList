889ac84096d8780bd16676da1d64708b7de3e528
Abhishek Verma <abhishek.verma@emirates.com>
2018-06-19 00:41:15 +0400

    Extand ParserHelper class functionality

--- FactsList/Service/Parser/ParserHelper.swift
+++ FactsList/Service/Parser/ParserHelper.swift
@@ -15,10 +15,48 @@ protocol Parceable {
 final class ParserHelper {
     
     /**
-     Function related to the FactsList Data parsing
+     Function related to the FactsList data parsing
      */
+    // FIXME: We can optimize this method later
     static func parse<T: Parceable>(data: Data, completion : (Result<[T], ErrorResult>) -> Void) {
         
+        if let value = String(data: data, encoding: String.Encoding.ascii) {
+            
+            if let jsonData = value.data(using: String.Encoding.utf8) {
+                
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] {
+                       
+                        if let arr = json["rows"] as? [[String: Any]] {
+                            debugPrint(arr)
+                            
+                            // init final result
+                            var finalResult : [T] = []
+                            
+                            for dictObject in arr {
+                                
+                                //check foreach dictionary if parseable
+                                switch T.parseObject(dictionary: dictObject) {
+                                case .failure(_):
+                                    continue
+                                case .success(let newModel):
+                                    finalResult.append(newModel)
+                                    break
+                                }
+                            }
+                            completion(.success(finalResult))
+                        }
+                    } else {  completion(.failure(.parser(string: "Error while parsing json data")))}
+                    
+                } catch {
+                    // can't parse json
+                    completion(.failure(.parser(string: "Error while parsing json data")))
+                }
+                
+            } else {  completion(.failure(.parser(string: "Error while parsing json data")))}
+        } else {
+            completion(.failure(.parser(string: "Error while parsing json data")))
+        }
     }
     
 }
