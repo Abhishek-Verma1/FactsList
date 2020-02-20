0a9c813203f56012d06d1b0e9b5e3b08f24338c3
Abhishek Verma <abhishek.verma@emirates.com>
2020-02-20 23:49:47 +0400

    AV: NYTimes Project Files Added.
    NYTimes Project Files Added.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var newStr = str
+
+str = "Hello, world"
+
+if str == newStr {
+    print("\(str) is equal to \(newStr)")
+} else {
+    print("\(str) is NOT equal to \(newStr)")
+}
